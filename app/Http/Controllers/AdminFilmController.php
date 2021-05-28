0cc09d38f14be1d6c73812943b52da49c7a0dfbf
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-05-28 23:35:19 +0300

    AddImageFilmShow

--- app/Http/Controllers/AdminFilmController.php
+++ app/Http/Controllers/AdminFilmController.php
@@ -25,12 +25,12 @@ class AdminFilmController extends Controller
     public function index()
     {
 
-        return Film::with('genrefilms','actorfilms','producerfilms')->get();
+        return Film::with('genrefilms','actorfilms','producerfilms','image')->get();
     }
  
     public function show(Film $film)
     {
-        return Film::where('id',$film->id)->with('genrefilms','actorfilms','producerfilms')->first();
+        return Film::where('id',$film->id)->with('genrefilms','actorfilms','producerfilms','image')->first();
     }
  
     public function store(Request $request)
